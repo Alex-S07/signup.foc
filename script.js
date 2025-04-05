2c029a522367512803a38c97097be1fbee8cde14
Alex Saju <alexsajued@gmail.com>
2025-04-05 16:09:29 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+function clicksubmitbutton(event) {
+    event.preventDefault(); 
+    const username = document.getElementById("username").value;
+    const welcomeMessage = document.getElementById('welcome-message');
+    welcomeMessage.textContent = 'Welcome, ' + username + '!';
+    welcomeMessage.style.display = 'block';
+}
